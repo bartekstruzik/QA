4ea71ebadaf29cae37be06765c6c30f59e064bb6
Struzik <BStruzik@poland.luxoft.com>
2018-02-14 13:25:47 +0100

    Fist commit

---
+++ IDDelector/IDSelector.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+class IDSelector
+{
+    static void Main()
+    {
+        string url = "http://testing.todvachev.com/selectors/class-name/";
+        string className = "testClass";
+
+
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl(url);
+
+        IWebElement element = driver.FindElement(By.ClassName(className));
+
+        Console.WriteLine(value: element.Text);
+
+        Thread.Sleep(3000);
+        driver.Quit();
+    }
+
+
+    private static void RedMessage(string v)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(v);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void GreenMessage(string v)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(v);
+        Console.ForegroundColor = ConsoleColor.White;
+
+    }
+}
+
---
+++ NameSelector/NameSelector.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+class NameSelector
+{
+    static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl("http://testing.todvachev.com/selectors/name/");
+
+        IWebElement element = driver.FindElement(By.Name("myName"));
+
+        if (element.Displayed)
+        {
+            GreenMessage("Yes, we can see it");
+        }
+        else
+        {
+            RedMessage("We cannot see it");
+        }
+
+        Thread.Sleep(4000);
+
+        driver.Quit();
+    }
+
+    private static void RedMessage(string v)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(v);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void GreenMessage(string v)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(v);
+        Console.ForegroundColor = ConsoleColor.White;
+
+    }
+}
+
