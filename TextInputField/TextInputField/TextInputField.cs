db65ae481f3fac5b63dd492ca97b6b5881af7ca5
bstruzik <bstruzik@BSTRUZIK>
2018-02-15 14:50:00 +0100

    TextInputField

---
+++ TextInputField/TextInputField/TextInputField.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+
+class TextInputField
+{
+    static IWebDriver driver = new ChromeDriver();
+    static IWebElement textBox;
+
+    static void Main()
+    {
+        string url = "http://testing.todvachev.com/special-elements/text-input-field/";
+
+        driver.Navigate().GoToUrl(url);
+
+        textBox = driver.FindElement(By.Name("username"));
+
+        Thread.Sleep(3000);
+        driver.Quit();
+
+    }
+}
+
